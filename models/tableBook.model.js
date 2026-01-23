ec9248effc541050404e3ed5bac5763efd443b6d
Viren Patadiya <102893049+VIREN2779@users.noreply.github.com>
2026-01-23 16:45:29 +0000

    refactor(tableBook.model.js): simplify schema definition by removing unnecessary object structure

--- models/tableBook.model.js
+++ models/tableBook.model.js
@@ -1,21 +1,11 @@
 const mongoose = require("mongoose");
 
-var tableBookSchema = new mongoose.Schema({
-    day: {
-        type: String
-    },
-    hours: {
-        type: String
-    },
-    fullName: {
-        type: String
-    },
-    phoneNumber: {
-        type: String
-    },
-    howManyPersons: {
-        type: String
-    }
+const tableBookSchema = new mongoose.Schema({
+    day: String,
+    hours: String,
+    fullName: String,
+    phoneNumber: Number,
+    howManyPersons: Number
 });
 
 module.exports = mongoose.model('tableBookDetail', tableBookSchema);
